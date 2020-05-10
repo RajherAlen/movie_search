0d348e3d92118939e387c1c3417e45b34a8d7d60
Alen <alen.rajher@gmail.com>
2020-05-10 21:23:51 +0200

    Updates

---
+++ precache-manifest.24ec93c103b89af0019f0304d61a2963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb365043a4eef6f07da831ccc64c86ee",
+    "url": "/movie_search/index.html"
+  },
+  {
+    "revision": "fe4da60ca58375ce1d02",
+    "url": "/movie_search/static/css/main.647114d9.chunk.css"
+  },
+  {
+    "revision": "a044ff03371dbf21cc4e",
+    "url": "/movie_search/static/js/2.7dacc4dd.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/movie_search/static/js/2.7dacc4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe4da60ca58375ce1d02",
+    "url": "/movie_search/static/js/main.eee4f046.chunk.js"
+  },
+  {
+    "revision": "e27714f3a010b275217a",
+    "url": "/movie_search/static/js/runtime-main.e69625b3.js"
+  }
+]);
